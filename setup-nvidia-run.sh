c70bcf1421bafe121ebf60e4e7c97a435679879f
Alex Ellis <alexellis2@gmail.com>
2024-03-20 09:12:09 +0100

    Update setup-nvidia-run.sh

--- setup-nvidia-run.sh
+++ setup-nvidia-run.sh
@@ -2,12 +2,21 @@
 
 # Additional helper script to be used via curl | sh if unable to use this action directly.
 
-curl -s -S -L -O https://us.download.nvidia.com/XFree86/Linux-x86_64/525.60.11/NVIDIA-Linux-x86_64-525.60.11.run \
-              && chmod +x ./NVIDIA-Linux-x86_64-525.60.11.run
-sudo -E ./NVIDIA-Linux-x86_64-525.60.11.run \
-                  --accept-license \
-                  --ui=none \
-                  --no-questions \
-                  --no-x-check \
-                  --no-check-for-alternate-installs \
-                  --no-nouveau-check
+RET=$(lspci | grep "3D controller" || true)
+if [ -n "$RET" ]
+then
+ curl -o ./NVIDIA-Linux-x86_64.run -s -S -L https://us.download.nvidia.com/tesla/550.54.15/NVIDIA-Linux-x86_64-550.54.15.run \
+    && chmod +x ./NVIDIA-Linux-x86_64.run
+else
+ curl -o ./NVIDIA-Linux-x86_64.run -s -S -L -O https://us.download.nvidia.com/XFree86/Linux-x86_64/525.60.11/NVIDIA-Linux-x86_64-525.60.11.run \
+   && chmod +x ./NVIDIA-Linux-x86_64.run
+fi
+chmod +x ./NVIDIA-Linux-x86_64.run
+
+sudo -E ./NVIDIA-Linux-x86_64.run \
+        --accept-license \
+        --ui=none \
+        --no-questions \
+        --no-x-check \
+        --no-check-for-alternate-installs \
+        --no-nouveau-check
